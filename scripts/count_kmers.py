aa1b10a41dbda08ac9ea25638df3d0bb02617665
jrvalcourt <jrvalcourt@gmail.com>
2020-04-28 14:59:51 -0400

    updated to make count dictionaries for all k < maxK so we can just do the calculation all at once

--- scripts/count_kmers.py
+++ scripts/count_kmers.py
@@ -7,13 +7,14 @@ from numbers import Number
 import percache
 
 ALPHABET = string.ascii_uppercase
-cache = percache.Cache("/tmp/my-cache")
+cache = percache.Cache("/tmp/python-cache")
 
 # remove all non-letter characters and make them all uppercase
 def clean_line(l):
+    l = l.decode('UTF-8')
     l = l.upper()
-    regex = re.compile(b'[^A-Z]')
-    l = regex.sub(b'', l)
+    regex = re.compile('[^A-Z]')
+    l = regex.sub('', l)
     return l
 
 # a generator that concatenates a bunch of files and yields them as a series 
@@ -35,52 +36,53 @@ def yield_kmers(files, K, verbose=False):
         print('\rDone.                        ')
 
 # insert the kmer into the count dictionary
-def add_to_count_dict(counts, kmer):
-    temp_counts = counts
+def add_to_count_dict(d, kmer):
+    temp = d
     for ii, c in enumerate(kmer):
-        if c in temp_counts:
+        if c in temp:
             if ii == len(kmer) - 1:
-                temp_counts[c] += 1
+                temp[c] += 1
             else:
-                temp_counts = temp_counts[c]
+                temp = temp[c]
         else:
             if ii == len(kmer) - 1:
-                temp_counts[c] = 1
+                temp[c] = 1
             else:
-                temp_counts[c] = {}
-                temp_counts = temp_counts[c]
+                temp[c] = {}
+                temp = temp[c]
 
 # returns a dictionary with the frequency counts of every kmer in the corpuses
 # contained across the given files
 @cache
 def count_kmers(files, K):
     counts = {}
-    counts_half = {}
+    for ii in range(K):
+        counts[ii+1] = {}
     total_kmers = 0
     for kmer in yield_kmers(files, K, verbose=True):
         total_kmers += 1
-        add_to_count_dict(counts, kmer)
-        add_to_count_dict(counts_half, kmer[:K//2])
-    return counts, total_kmers, counts_half
+        for ii in range(K):
+            add_to_count_dict(counts[ii+1], kmer[:ii+1])
+    return counts, total_kmers
 
 # retrieve the count associated with the given kmer
-def get_count(counts, kmer):
-    temp_counts = counts
+def get_count(d, kmer):
+    temp = d
     for c in kmer:
-        temp_counts = temp_counts[c]
-    assert isinstance(temp_counts, Number)
-    return temp_counts
+        temp = temp[c]
+    assert isinstance(temp, Number)
+    return temp
     
 # score the kmers from a given file
-def score_kmers(files, K, counts, total_kmers, counts_half):
+def score_kmers(files, K, counts, total_kmers):
     for kmer in yield_kmers(files, K):
-        first_half = kmer[:K//2]
+        first_half  = kmer[:K//2]
         second_half = kmer[K//2:]
         
         # retrieve the number of times we saw the full kmer and each half
-        n_full = get_count(counts,      kmer)
-        n_1    = get_count(counts_half, first_half)
-        n_2    = get_count(counts_half, second_half)
+        n_full = get_count(counts[K],    kmer)
+        n_1    = get_count(counts[K//2], first_half)
+        n_2    = get_count(counts[K//2], second_half)
 
         # figure out the probabilities
         # we have to work in log space for probs to prevent underflow
@@ -89,14 +91,11 @@ def score_kmers(files, K, counts, total_kmers, counts_half):
         log_p_2    = np.log(n_2)    - np.log(total_kmers)
         log_p_ratio = log_p_kmer - (log_p_1 + log_p_2)
 
-        if log_p_ratio < 0:
-            print(kmer, log_p_ratio)
-
-K = int(sys.argv[1])
-if not K % 2 == 0:
+maxK = int(sys.argv[1])
+if not maxK % 2 == 0:
     sys.exit()
+print(f"Using maxK={maxK}")
 test_file = sys.argv[2]
 train_files = sys.argv[3:]
-counts, total_kmers, counts_half = count_kmers(train_files, K)
-score_kmers(test_file, K, counts, total_kmers, counts_half)
-
+counts, total_kmers = count_kmers(train_files, maxK)
+score_kmers([test_file], maxK, counts, total_kmers)
